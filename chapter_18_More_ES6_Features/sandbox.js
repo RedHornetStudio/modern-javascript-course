966a43c070942bb5c4eb484dc5fc63dd3209691a
RedHornetStudio <51362978+RedHornetStudio@users.noreply.github.com>
2020-03-23 14:29:58 +0200

    modified:   chapter_18_More_ES6_Features/sandbox.js

--- chapter_18_More_ES6_Features/sandbox.js
+++ chapter_18_More_ES6_Features/sandbox.js
@@ -19,3 +19,56 @@ const person = { name: 'shaun', age: 30, job: 'net ninja' };
 const personClone = { ...person, location: 'manchester' };
 console.log(person);
 console.log(personClone);
+
+///////////////////////////////////////////// sets
+const namesArray = ['ryu', 'shun-li', 'ryu', 'shaun'];
+console.log(namesArray);
+
+// const nameSet = new Set(['ryu', 'shun-li', 'ryu', 'shaun']);
+// const nameSet = new Set(namesArray);
+// console.log(nameSet);
+
+// const uniqueNames = [...nameSet];
+const uniqueNames = [...new Set(namesArray)];
+console.log(uniqueNames);
+
+const ages = new Set();
+ages.add(30);
+ages.add(25).add(20);
+ages.add(25);
+ages.delete(25);
+
+console.log(ages, ages.size);
+console.log(ages.has(30), ages.has(25));
+
+ages.clear();
+console.log(ages);
+
+const ninjas = new Set([
+    {name: 'shaun', age: 30},
+    {name: 'crystal', age: 29},
+    {name: 'chun-li', age: 32}
+])
+
+ninjas.forEach(ninja => {
+    console.log(ninja.name, ninja.age);
+});
+
+///////////////////////////////////////////// symbols
+const symboleOne = Symbol('a generic name');
+const symboleTwo = Symbol('a generic name');
+
+console.log(symboleOne, symboleTwo, typeof symboleOne);
+console.log(symboleOne == symboleTwo);
+
+const ninja = {};
+
+ninja.age = 30;
+ninja['belt'] = 'orange';
+ninja.belt = 'balck';
+
+ninja[symboleOne] = 'ryu';
+ninja[symboleTwo] = 'shaun';
+
+console.log(ninja);
+console.log(ninja[symboleOne]);
